45026ad791ffe7cea7cedae8700cf7f585dc3ac7
Preethi Goli <golip@purdue.edu>
2022-01-12 16:39:45 -0500

    Initial Commit

---
+++ New Tip Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New Tip Calculator
+//
+//  Created by Preethi Goli on 1/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
