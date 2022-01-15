d7c2be4bcf3bd0829a6a7b09060fb630941d5bf1
Preethi Goli <golip@purdue.edu>
2022-01-15 13:58:21 -0500

    Added .gitignore and gitattributes

--- New Tip Calculator/ViewController.swift
+++ New Tip Calculator/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textlabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipAmount: UISegmentedControl!
+    @IBOutlet weak var inputField: UITextField!
+    
+    @IBOutlet weak var TipLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(inputField.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.2, 0.25]
+        let tip = bill * tipPercentages[tipAmount.selectedSegmentIndex]
+        let total = bill + tip
+        totalLabel.text = String(format: "$%.2f", total)
+        TipLabel.text = String(format: "$%.2f", tip)
+        
+    }
+    
 }
 
